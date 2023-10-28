f4860362d5eabf2b185595fb1c986511ea9e5a6a
masabiii <mhadyannurabi@gmail.com>
2023-10-28 13:25:41 +0700

    first commit

--- SoalA/Soal2A/soal2a.php
+++ SoalA/Soal2A/soal2a.php
@@ -9,14 +9,24 @@
     <?php
     include 'koneksi.php';
 
-    $sql = "SELECT h.hobi, COUNT(p.id) as jumlah_orang
-            FROM hobi h
-            LEFT JOIN person p ON h.person_id = p.id
-            GROUP BY h.hobi
-            ORDER BY jumlah_orang DESC";
-    
+    if (isset($_GET['search_hobby'])) {
+        $search_hobby = mysqli_real_escape_string($conn, $_GET['search_hobby']);
+        $sql = "SELECT h.hobi, COUNT(p.id) as jumlah_orang
+                FROM hobi h
+                LEFT JOIN person p ON h.person_id = p.id
+                WHERE h.hobi LIKE '%$search_hobby%'
+                GROUP BY h.hobi
+                ORDER BY jumlah_orang DESC";
+    } else {
+        $sql = "SELECT h.hobi, COUNT(p.id) as jumlah_orang
+                FROM hobi h
+                LEFT JOIN person p ON h.person_id = p.id
+                GROUP BY h.hobi
+                ORDER BY jumlah_orang DESC";
+    }
+
     $result = mysqli_query($conn, $sql);
-    
+
     if (mysqli_num_rows($result) > 0) {
     ?>
     <table border='1'>
@@ -38,5 +48,11 @@
 
     mysqli_close($conn);
     ?>
+    
+    <form method="GET">
+        <label for="search_hobby">Cari Berdasarkan Hobi:</label>
+        <input type="text" name="search_hobby" id="search_hobby">
+        <input type="submit" value="Cari">
+    </form>
 </body>
 </html>
